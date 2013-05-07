49ebe20adf2adbbebdceff874ee090605b2cf55a
Fumito Mizuno <ounziw@gmail.com>
2013-05-07 12:50:04 +0900

    jaform is now multi lingual

---
+++ README.md
@@ -0,0 +1,23 @@
+jpaddress
+=========
+
+Address form of Japan
+
+
+This is an extension for Novius OS form.
+
+This allows to add a set of forms (zip1, zip2, prefecture, address1, and address2) with one click.
+
+
+Usage
+=====
+
+Put form.config.php into local/config/apps/noviusos_form/controller/admin folder.
+Put jaform.lang.php into local/lang/ja folder.
+(If these folders do not exist, please create them.)
+
+
+License
+=======
+
+AGPL ver.3.0 or later
---
+++ config/apps/noviusos_form/controller/admin/form.config.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * NOVIUS OS - Web OS for digital communication
+ *
+ * @copyright  2011 Novius
+ * @license    GNU Affero General Public License v3 or (at your option) any later version
+ *             http://www.gnu.org/licenses/agpl-3.0.html
+ * @link http://www.novius-os.org
+ */
+
+Nos\I18n::current_dictionary(array('local::jaform', 'noviusos_form::common'));
+
+$pref = array(
+    __('hokkaido'), __('aomori'), __('iwate'), __('miyagi'), __('akita'), __('yamagata'), __('fukushima'),
+    __('ibaraki'), __('tochigi'), __('gunma'), __('saitama'), __('chiba'), __('tokyo'), __('kanagawa'),
+    __('niigata'), __('toyama'), __('ishikawa'), __('fukui'), __('yamanashi'), __('nagano'),
+    __('gifu'), __('shizuoka'), __('aichi'), __('mie'),
+    __('shiga'), __('kyoto'), __('osaka'), __('hyogo'), __('nara'), __('wakayama'),
+    __('tottori'), __('shimane'), __('okayama'), __('hiroshima'), __('yamaguchi'),
+    __('tokushima'), __('kagawa'), __('ehime'), __('kochi'),
+    __('fukuoka'), __('saga'), __('nagasaki'), __('kumamoto'), __('oita'), __('miyazaki'), __('kagoshima'), __('okinawa'));
+
+return array(
+    'fields_meta' => array(
+        'special' => array(
+            'jpaddress' => array(
+                'icon' => 'static/apps/noviusos_form/img/fields/address.png',
+                'title' => __('ja_address'),
+                'definition' => array(
+                    'layout' => "postal1=1,postal2=1\npref=1\nline_1=4\nline_2=4",
+                    'fields_list' => array(
+                        'postal1' => array(
+                            'field_type' => 'number',
+                            'field_label' => __('zip1'),
+                        ),
+                        'postal2' => array(
+                            'field_type' => 'number',
+                            'field_label' => __('zip2'),
+                        ),
+                        'pref' => array(
+                            'field_type' => 'select',
+                            'field_label' => __('prefecture'),
+                            'field_choices' => implode("\n", $pref),
+                        ),
+                        'line_1' => array(
+                            'field_type' => 'text',
+                            'field_label' => __('First address line:'),
+                        ),
+                        'line_2' => array(
+                            'field_type' => 'text',
+                            'field_label' => __('Second address line:'),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+);
